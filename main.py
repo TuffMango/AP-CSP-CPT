86b89a6a744294f87f30b54d983d59e84576ac5b
TuffMango <therealgeorege@gmail.com>
2026-04-16 16:25:43 +0000

    stufff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-
-# test fork
-
+#fork test
 deck = [
     "One of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts", 
     "Six of Hearts", "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", 
@@ -23,39 +21,74 @@ import math
 import sys
 import random
 
-print("Welcome to Texas Hold em' Poker Simulator \n")       # beginning sequence
+print("Welcome to the BlackJack Simulator \n")       # beginning sequence
 print("Dealing cards...\n")
 time.sleep(2)
 
 def playercards():                      #Me and Nathan Together, deals cards to user
-    print("Your cards \n")
+    print("\nYour cards")
     playercard1 = random.choice(deck)
     deck.remove(playercard1)
-    print(playercard1)
     playercard2 = random.choice(deck)
-    print(playercard2)
     deck.remove(playercard2)
-   
+    print(f"You: {playercard1}, {playercard2}\n")
 
 
-playercards()
 def bot1():
     botcard1 = random.choice(deck)
     deck.remove(botcard1)
     botcard2 = random.choice(deck)
     deck.remove(botcard2)
+    print(f"Bot cards: {botcard1}, {botcard2}\n")
 
+def dealer():
+    dealercard1 = random.choice(deck)
+    deck.remove(dealercard1)
+    dealercard2hidden = random.choice(deck)
+    deck.remove(dealercard2hidden)
+    print(f"Dealer: {dealercard1}, Hidden")
+
+dealer()
+playercards()
 bot1()
 
-def first3():
-    card1 = random.choice(deck)
-    deck.remove(card1)
-    card2 = random.choice(deck)
-    deck.remove(card2)
-    card3 = random.choice(deck)
-    deck.remove(card3)
-    print("\tCards on the table \n\t-----------------------")
-    print(f"{card1}, {card2}, {card3}")
+
+player_money = 500
+pot = 0
+player_bet = int(input("Please place your bet(max 500):  \n"))
+while player_bet < 1 :
+    player_bet = int(input("Please place your bet:  \n"))
+pot = player_bet + pot
+bot1_money = random.randint(1, 300)
+print(f"Amount on table is {pot} \n")
+time.sleep(2)
+print(f"You have ${player_money} \n")
+time.sleep(4)
+print("Bot One's turn...")
+time.sleep(3)
+pot = pot + bot1_money
+print(f"Bot 1 added {bot1_money}, current pot is now {pot} \n")
+time.sleep(4)
+print("Bot Two's turn...")
+bot2_money = random.randint(1, 300)
+pot = pot + bot2_money
+print(f"Bot Two added {bot2_money}, current pot is now {pot} \n")
+
+
+
+    
     
 
-first3()
+    
+
+
+
+
+
+
+            
+
+        
+    
+
+
